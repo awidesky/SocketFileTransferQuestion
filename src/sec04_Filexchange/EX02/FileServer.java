9fa573bac52f79a36d48d17c54245df402325187
Eugene Hong <awidesky@naver.com>
2024-02-14 14:00:09 +0900

    original code from the question

--- src/sec04_Filexchange/EX02/FileServer.java
+++ src/sec04_Filexchange/EX02/FileServer.java
@@ -6,50 +6,45 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class FileServer {
-	public static void run() {
+	public static void main(String[] args) {
 
 		System.out.println("  SERVER ");
 		ServerSocket ssocket = null;
 		try {
 			ssocket = new ServerSocket(10000);
 		} catch (IOException e) {
-			e.printStackTrace();
-			System.out.println("[Server]  port not avaliable ");
+			System.out.println(" port not avaliable ");
 		}
 
-		System.out.println("[Server]  stand by Server ");
+		System.out.println(" stand by client ");
 		try {
 			Socket socket = ssocket.accept();
-			System.out.println("[Server]  Server connection complete  ");
-			System.out.println("[Server] Server  ::  " + socket.getInetAddress() + "." + socket.getPort() + "  "
+			System.out.println(" client connection complete  ");
+			System.out.println("client  ::  " + socket.getInetAddress() + "." + socket.getPort() + "  "
 					+ socket.getLocalSocketAddress());
 
 			DataInputStream dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 			DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 			String filename = dis.readUTF();
-			System.out.println("[Server]  receive file  " + filename);
+			System.out.println(" receive fiel  " + filename);
 
-			File outfile = new File("D:\\JAVA_Coding_Study\\file\\" + filename);
-			outfile.getParentFile().mkdirs();
-			outfile.delete(); outfile.createNewFile();
+			File outfile = new File("C:\\JAVA_Coding_Study\\file\\" + filename);
 			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(outfile));
 
 			byte[] data = new byte[1024];
-			int len, cnt = 1;
+			int len;
 			while ((len = dis.readInt()) != -1) {
-				int sent = dis.read(data, 0, len);
-				System.out.println("[Server] packet #" + cnt++ + " : " + sent + "byte");
+				dis.read(data, 0, len);
 				bos.write(data, 0, len);
 				bos.flush();
 			}
-			System.out.println("[Server]  file receiver complete ");
-			dos.writeUTF("[Server]  file receive complete ");
+			System.out.println(" file receiver complete ");
+			dos.writeUTF(" file receive complete ");
 			dos.flush();
 
 			dis.close();
 			dos.close();
 			bos.flush();
-			bos.close();
 			if (socket != null)
 				socket.close();
 		} catch (UnknownHostException e) {
